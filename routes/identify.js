82ab0935dda402fd5d89a323b9590e466fc82fd4
Tankala Kiran Narayana <106546149+kirantankala@users.noreply.github.com>
2025-05-25 14:24:58 +0530

    Update identify.js

--- routes/identify.js
+++ routes/identify.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { identifyContact } = require('../controllers/identifyController');
-
+// we use post api to upload the user in the db
 router.post('/identify', identifyContact);
 
 module.exports = router;
